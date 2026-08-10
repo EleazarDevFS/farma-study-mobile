1fcfd7f7efd52ba986c3335c5dfc55841e112c66
EleazarDev <eleazarjarquincv@gmail.com>
2026-08-10 16:53:02 -0600

    fix: Resolve notation Entity

--- app/src/main/java/com/example/farmastudy/data/local/entity/QuestionEntity.kt
+++ app/src/main/java/com/example/farmastudy/data/local/entity/QuestionEntity.kt
@@ -1,8 +1,9 @@
 package com.example.farmastudy.data.local.entity
 
 import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+@Entity(tableName = "questions")
 data class QuestionEntity(
     @PrimaryKey val id: Int,
     val question: String,
