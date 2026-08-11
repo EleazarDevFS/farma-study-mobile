94ee52338faf3b585ccb0329c6014d252940cc0a
EleazarDev <eleazarjarquincv@gmail.com>
2026-08-11 16:19:29 -0600

    feat: add user repository

---
+++ app/src/main/java/com/example/farmastudy/data/repository/UserRepository.kt
@@ -0,0 +1,41 @@
+package com.example.farmastudy.data.repository
+
+import at.favre.lib.crypto.bcrypt.BCrypt
+import com.example.farmastudy.data.domain.model.AuthResult
+import com.example.farmastudy.data.local.dao.UserDao
+import com.example.farmastudy.data.local.entity.UserEntity
+
+class UserRepository(private val userDao: UserDao) {
+    suspend fun register(
+        username: String,
+        name: String,
+        age: Int,
+        email: String,
+        password: String
+    ): AuthResult {
+        if (userDao.finByUsername(username) != null) {
+            return AuthResult(success = false, message = "Username ya existe, elige otro")
+        }
+        val hash = BCrypt.withDefaults().hashToString(12, password.toCharArray())
+        userDao.insert(
+            UserEntity(
+                username = username,
+                password = hash,
+                email = email,
+                age = age,
+                gender = null
+            )
+        )
+        return AuthResult(success = true)
+    }
+    suspend fun login(username: String, password: String): AuthResult {
+        val user = userDao.finByUsername(username)
+            ?: return AuthResult(success = false, message = "Usuario no encontrado")
+        val verifed = BCrypt.verifyer().verify(password.toCharArray(), user.password).verified
+        return if (verifed){
+            AuthResult(success = true, username = user.username)
+        } else{
+            AuthResult(success = false, message = "Contraseña incorrecta")
+        }
+    }
+}
